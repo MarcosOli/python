deac3dec825983a3f0be742cae3d4549c412307e
MarcosOli <1990mar@gmail.com>
2019-05-08 17:06:59 -0300

    Add files via upload

---
+++ Optimize Image/otimizador.py
@@ -0,0 +1,30 @@
+import tinify
+import xlrd
+import os
+from urllib.parse import urlsplit
+import requests
+tinify.key = '5p5UxEJn7z5jYohyzUzpqeCJ3Ymbadxj'
+
+#tinify.proxy = "http://mvldOliveira@timbrasil.com.br:1B2e3l4l5a@192.168.0.1:8080"
+
+
+pre = os.path.dirname(os.path.realpath(__file__))
+fname = 'links.xlsx'
+path = os.path.join(pre, fname)
+book = xlrd.open_workbook(path)
+print("Número de abas: ", book.nsheets)
+print("Nomes das Planilhas:", book.sheet_names())
+sh = book.sheet_by_index(0)
+print(f'Nome: {sh.name}, Linhas: {sh.nrows}, Colunas: {sh.ncols}')
+for rx in range(sh.nrows):
+
+    linha = str(sh.row(rx))
+    lixo1, url, lixo2 = linha.split("'")
+    parts = urlsplit(url)
+    paths = parts.path.split('/')
+    page = requests.get(url)
+    print(url)
+    statusCode = page.status_code
+    print('Status Code:',statusCode)
+    source = tinify.from_url(url)
+    source.to_file(paths[-1])
